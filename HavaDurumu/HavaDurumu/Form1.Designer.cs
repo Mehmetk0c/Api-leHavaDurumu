cadfd39bd34a508c29294b06f4d993927fd4e9ba
Mehmet KOÇ <107946462+Mehmetk0c@users.noreply.github.com>
2022-06-21 21:13:26 +0300

    HavaDurumuUygulaması
    C# form ve api ile basit bir  hava durumu uygulaması

---
+++ HavaDurumu/HavaDurumu/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace HavaDurumu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "İstanbul",
+            "Ankara",
+            "Kayseri"});
+            this.comboBox1.Location = new System.Drawing.Point(195, 35);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Şehir";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(195, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Hava Durumu";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 207);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ HavaDurumu/HavaDurumu/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace HavaDurumu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string api = "ff5384f572da1653f3f6e115696950fa";
+        private void button1_Click(object sender, EventArgs e)
+        {
+           string key = comboBox1.Text;
+           string connection= "http://api.openweathermap.org/data/2.5/weather?q="+key.ToLower()+"&mode=xml&lang=tr&units=metric&appid="+api;
+           XDocument weathear = XDocument.Load(connection);
+           var temp = weathear.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            double temp2 = Math.Round(Convert.ToDouble(temp.Replace(".", ",")),1);
+            var weath = weathear.Descendants("clouds").ElementAt(0).Attribute("name").Value;
+
+            MessageBox.Show(key + " İçin Beklenen sıcaklık " + temp2 + "°C ve " + weath.ToString()+".");
+            
+        }
+    }
+}
